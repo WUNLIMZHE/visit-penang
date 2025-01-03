49ff7ba785149ff7cd00504cefa4f69c33f418fd
xinsheng04 <freddy161204@gmail.com>
2025-01-03 12:00:10 +0800

    Added different pages for Food, Tourism and Hotels and routing capabilities for them

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import Navbar from "../Component/Navbar/Navbar"
+import Brochure from '../Component/Brochure/Brochure'
+import Program from '../Component/Program/Program'
+import Title from '../Component/Title/Title'
+import About from '../Component/About/About'
+export default function Home (){
+    return(    
+        <>
+            <Navbar/>
+            <Brochure/>
+            <div className='container'>
+            <Title subTitle='What we have' title='Penang is not just a place to visit, it is a living experience, a feast for the senses, and a celebration of coexistence, where every step tells a story.'/>
+            <Program/>
+            <About/>
+            <Title subTitle='Gallery' title='More about Penang'/>
+            </div>
+        </>
+    );
+}
