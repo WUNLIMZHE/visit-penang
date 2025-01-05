1535265845791e0841cb642849406a5025be3016
WUN LIM ZHE <wunlimzhe2004@gmail.com>
2025-01-05 15:44:44 +0800

    moved @import statement to the first line of the index.css

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -11,7 +11,7 @@ function Card(props) {
           alt={props.alt}
         />
       </a>
-      <div className="p-5">
+      <div className="p-5 grid">
         <a href="#">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {props.name}
@@ -21,7 +21,7 @@ function Card(props) {
           {props.description}
         </p>
         <div className="flex gap-x-3">
-          <div className="grid gap-1 items-center text-theme-800">
+          <div className="grid gap-1 items-center text-theme-800 content-start mt-1">
             <svg
               width="1em"
               height="1em"
@@ -37,6 +37,13 @@ function Card(props) {
               </symbol>
               <use xlinkHref="#ai:ci:location"></use>
             </svg>
+          </div>
+          <div className="grid gap-1 items-center font-bold text-lg">
+            <span>{props.location}</span>
+          </div>
+        </div>
+        <div className="flex gap-x-3 mt-2">
+          <div className="grid gap-1 items-center text-theme-800 content-start mt-1">
             <svg
               width="1em"
               height="1em"
@@ -57,40 +64,36 @@ function Card(props) {
               <use xlinkHref="#rating:star"></use>
             </svg>
           </div>
-          <div className="grid gap-1 grid-rows-3 items-center font-bold text-lg">
-            <span>{props.location}</span>
+          <div className="grid gap-1 items-center font-bold text-lg content-start">
             <span>{props.rating}/5.0</span>
           </div>
         </div>
-        <a
-          href="#"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Read more
-          <svg
-            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 10"
+        <div className="mt-auto">
+          <a
+            href="#"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-3"
           >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 5h12m0 0L9 1m4 4L9 9"
-            />
-          </svg>
-        </a>
+            Read more
+            <svg
+              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </a>
+        </div>
       </div>
     </div>
   );
 }
 
 export default Card;
-
-// Validate the 'data' prop
-Card.propTypes = {
-    props: PropTypes.array.isRequired,  // Ensure data is an array and is required
-};
