671d2e5306b257335fab1a181461b2893060bf30
WUN LIM ZHE <wunlimzhe2004@gmail.com>
2025-01-06 22:05:52 +0800

    Fix navbar and layout issue

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -11,10 +11,10 @@ function Card({onClick, ...props}) {
         alt={props.alt}
       />
       <div className="p-5 flex flex-col flex-grow">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-black dark:text-black">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-black">
           {props.name}
         </h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        <p className="mb-3 font-normal text-gray-700">
           {props.description}
         </p>
         <div className="flex gap-x-3">
@@ -68,7 +68,7 @@ function Card({onClick, ...props}) {
         {/* Push the button to the bottom */}
         <div className="mt-auto">
           <a
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-3" onClick={() => onClick(props.id)}
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 mt-3" onClick={() => onClick(props.id)}
           >
             Read more
             <svg
