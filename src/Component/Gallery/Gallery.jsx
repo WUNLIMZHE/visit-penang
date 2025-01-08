b57fe9272e85e1c8d11e92441d291ed5d0e8c1a8
unknown <zh2eng2022>
2025-01-08 10:09:50 +0800

    Footer

--- src/Component/Gallery/Gallery.jsx
+++ src/Component/Gallery/Gallery.jsx
@@ -7,7 +7,7 @@ import img6 from '../../../public/23little-india.webp'
 export default function Gallery(){
     return (
         <div className="flex justify-center data-center min-h-screen">
-        <section className="mt-[128px] grid md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-8 justify-data-center mb-10">
+        <section className="mt-[128px] grid md:grid-cols-2 lg:grid-cols-3 gap-x-3 gap-y-4 justify-data-center mb-10">
           <img class="w-full h-[220px] rounded-[10px]" src={img1}/>
           <img class="w-full h-[220px] rounded-[10px]" src={img2}/>
           <img class="w-full h-[220px] rounded-[10px]" src={img3}/>
