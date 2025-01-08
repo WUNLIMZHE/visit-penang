849e367b7641b05d7f8d415983522c2d17e74ba6
WUN LIM ZHE <wunlimzhe2004@gmail.com>
2025-01-08 09:51:54 +0800

    Change image path in galery.jsx

--- src/Component/Gallery/Gallery.jsx
+++ src/Component/Gallery/Gallery.jsx
@@ -1,19 +1,19 @@
-import img1 from '../../../public/17escape-penang.webp'
-import img2 from '../../../public/18entopia.webp'
-import img3 from '../../../public/20bird-park.webp'
-import img4 from '../../../public/21tech-dome.webp'
-import img5 from '../../../public/22wonderfood-museum.webp'
-import img6 from '../../../public/23little-india.webp'
+import img1 from '/17escape-penang.webp'
+import img2 from '/18entopia.webp'
+import img3 from '/20bird-park.webp'
+import img4 from '/21tech-dome.webp'
+import img5 from '/22wonderfood-museum.webp'
+import img6 from '/23little-india.webp'
 export default function Gallery(){
     return (
         <div className="flex justify-center data-center min-h-screen">
         <section className="mt-[128px] grid md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-8 justify-data-center mb-10">
-          <img class="w-full h-[220px] rounded-[10px]" src={img1}/>
-          <img class="w-full h-[220px] rounded-[10px]" src={img2}/>
-          <img class="w-full h-[220px] rounded-[10px]" src={img3}/>
-          <img class="w-full h-[220px] rounded-[10px]" src={img4}/>
-          <img class="w-full h-[220px] rounded-[10px]" src={img5}/>
-          <img class="w-full h-[220px] rounded-[10px]" src={img6}/>
+          <img className="w-full h-[220px] rounded-[10px]" src={img1}/>
+          <img className="w-full h-[220px] rounded-[10px]" src={img2}/>
+          <img className="w-full h-[220px] rounded-[10px]" src={img3}/>
+          <img className="w-full h-[220px] rounded-[10px]" src={img4}/>
+          <img className="w-full h-[220px] rounded-[10px]" src={img5}/>
+          <img className="w-full h-[220px] rounded-[10px]" src={img6}/>
         </section>
       </div>
     );
